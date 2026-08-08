1f9c06a04bbd1a036aa3430e33fc5bf680d27ce2
atikkeeee <hsumiya935@gmail.com>
2026-08-08 12:44:43 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+function login(){
+
+    const pass=document.getElementById("password").value;
+
+    if(pass==="Snehatik"){
+
+        document.getElementById("loginPage").classList.add("hidden");
+        document.getElementById("homePage").classList.remove("hidden");
+
+    }else{
+
+        alert("Wrong Password ❤️");
+
+    }
+
+}
+
+function openLetter(){
+
+    document.getElementById("homePage").classList.add("hidden");
+    document.getElementById("letterPage").classList.remove("hidden");
+
+}
+
+function finalSurprise(){
+
+    document.getElementById("letterPage").classList.add("hidden");
+    document.getElementById("finalPage").classList.remove("hidden");
+
+}
+
+setInterval(()=>{
+
+    let heart=document.createElement("div");
+
+    heart.className="heart";
+
+    heart.innerHTML="❤️";
+
+    heart.style.left=Math.random()*100+"vw";
+
+    heart.style.fontSize=(18+Math.random()*25)+"px";
+
+    document.body.appendChild(heart);
+
+    setTimeout(()=>{
+
+        heart.remove();
+
+    },7000);
+
+},300);
