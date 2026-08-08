71dd8d0ed19d5fe47cfaafac65b7fb4163d8900f
atikkeeee <hsumiya935@gmail.com>
2026-08-08 21:40:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -24,11 +24,11 @@ function finalSurprise() {
 }
 
 const photos = [
-  "images/1.jpg",
-  "images/2.jpg",
-  "images/3.jpg",
-  "images/4.jpg",
-  "images/5.jpg"
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg"
 ];
 
 let current = 0;
